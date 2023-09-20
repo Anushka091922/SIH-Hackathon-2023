4df3133557e11fe6118b49a54df97cc65703d614
VinayK2003 <114322843+VinayK2003@users.noreply.github.com>
2023-09-20 18:57:07 +0530

    Add files via upload

---
+++ login-lawyer/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { createRoot } from "react-dom/client";
+import './index.css';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import App from './App';
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
